abb604c3bc48f4f2cfa23a81310f58ca36a27497
Salin Dhakal <salindhakal7@gmail.com>
2024-11-04 19:14:17 +0545

    First Commit

---
+++ src/components/HomeCards.jsx
@@ -0,0 +1,35 @@
+import Card from "./Card";
+import { Link } from "react-router-dom";
+
+const HomeCards = () => {
+  return (
+    <section className="py-4">
+      <div className="container-xl lg:container m-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+          <Card>
+            <h2 className="text-2xl font-bold">Rating Prediction</h2>
+            <p className="mt-2 mb-4">Prediction at your finger tip</p>
+            <Link
+              to="/rating"
+              className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+            >
+              View Ratings
+            </Link>
+          </Card>
+          <Card bg="bg-indigo-100">
+            <h2 className="text-2xl font-bold">Custom</h2>
+            <p className="mt-2 mb-4">Generate your own player</p>
+            <Link
+              to="/custom"
+              className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+            >
+              Custom player
+            </Link>
+          </Card>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeCards;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import HomeCards from "../components/HomeCards";
+import Top from "../components/Top";
+
+const HomePage = () => {
+  return (
+    <>
+      <Top
+        title="Football Player Rating"
+        subtitle="Predictions at your Finger Tips"
+      />
+      <HomeCards />
+    </>
+  );
+};
+
+export default HomePage;
